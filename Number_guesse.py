cc3c958f548c6f0107ba2081bc4210b45a0c5155
ismail137 <iwahbi932@gmail.com>
2024-12-06 16:21:39 +0100

    cheking the range of number

--- Number_guesse.py
+++ Number_guesse.py
@@ -6,9 +6,14 @@ guesses=[]
 gameover=False
 
 while not gameover:
-    guesse = int(input("Enter your guesed numb between 1 to 100     "    ))
-    print()
-    if guesse != num:
+    
+    guesse = input("Enter your guesse between 1 and 100")
+
+    if guesse.isdigit():
+        guesse = int(guesse)
+        if guesse<1 or guesse>100:
+            print ("your input out of range")
+            break
         if guesse>num:
             print(' too hight  please try again.')
             guesses.append(guesse)
@@ -20,6 +25,10 @@ while not gameover:
             print(f"GAME OVER you have up of {minimum_guesses} tries and the correct num is {num}" )
             print(guesses)
             gameover=True
+        
+            print()
+    else:
+        print("invalid input your input must be number between 1 and 100")
     if guesse==num:
         print(f"YOU win good work your right you guess {guesse} and the computer number is {num}")
         guesses.append(guesse)
@@ -27,4 +36,3 @@ while not gameover:
         print(guesses)
 
         gameover=True
-       
